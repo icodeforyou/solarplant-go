530ceb9b1924bd2ca96747dec18e7127efce1e4a
Andreas Assermark <{ID}+{username}@users.noreply.github.com>
2025-02-22 17:40:35 +0100

    Next-day energy prices from elprisetjustnu

--- elprisetjustnu/elprisetjustnu.go
+++ elprisetjustnu/elprisetjustnu.go
@@ -28,9 +28,24 @@ func New(area string) ElPrisetJustNu {
 }
 
 func (e ElPrisetJustNu) GetEnergyPrices(ctx context.Context) ([]types.EnergyPrice, error) {
-	now := time.Now()
+	t := time.Now()
+	today, err := e.getEnergyPrices(ctx, t.Year(), int(t.Month()), t.Day())
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch prices for today: %w", err)
+	}
+
+	t = t.AddDate(0, 0, 1)
+	tomorrow, err := e.getEnergyPrices(ctx, t.Year(), int(t.Month()), t.Day())
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch prices for tomorrow: %w", err)
+	}
+
+	return append(today, tomorrow...), nil
+}
+
+func (e ElPrisetJustNu) getEnergyPrices(ctx context.Context, y, m, d int) ([]types.EnergyPrice, error) {
 	url := fmt.Sprintf("https://www.elprisetjustnu.se/api/v1/prices/%d/%02d-%02d_%s.json",
-		now.Year(), now.Month(), now.Day(), e.area)
+		y, m, d, e.area)
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
@@ -42,6 +57,10 @@ func (e ElPrisetJustNu) GetEnergyPrices(ctx context.Context) ([]types.EnergyPric
 		return nil, fmt.Errorf("failed to fetch prices: %w", err)
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return []types.EnergyPrice{}, nil
+	}
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
